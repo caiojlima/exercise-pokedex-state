1cf77e95c4d46ab4d3bb93fa899cf64e4711faf6
Caio Lima <caiojlimah@gmail.com>
2021-10-07 21:21:39 -0300

    Adicionando desestruturações no código

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -14,43 +14,34 @@ class Pokedex extends React.Component {
   }
 
   nextPokemon() {  
-    this.setState(({ filter, index }, _props) => {
-      return (filter.length - 1 === index) ? { index: 0 } : { index: index + 1 }
+    this.setState(({ filter: { length }, index }) => {
+      return (length - 1 === index) ? { index: 0 } : { index: index + 1 }
     })
   }
 
   setPokemonByType = ( { target: { className }} ) => {
-    this.setState((_previousState, _props) => {
-      if (className === 'all') {
-        return {
-          index: 0,
-          filter: data,
-        }
-      }
-      return {
-        index: 0,
-        filter: data.filter(({ type }) => type.toLowerCase() === className),
-      }
+    this.setState(() => {
+      return (className === 'all') ? { index: 0, filter: data, } : { index: 0, filter: data.filter(({ type }) => type.toLowerCase() === className), }
     });
   }
 
   render() {
-    const { state: { filter, index } } = this;
+    const { state: { filter, index }, setPokemonByType, nextPokemon } = this;
     return (
       <div className="main-content">
       <Pokemon pokemon={filter[index]} />
       <div className="buttons-container">
         <div className="type-btn-container">
-          <button className="all" onClick={ this.setPokemonByType }>All</button>
-          <button className="electric" onClick={ this.setPokemonByType }>Eletric</button>
-          <button className="fire" onClick={ this.setPokemonByType }>Fire</button>
-          <button className="bug" onClick={ this.setPokemonByType }>Bug</button>
-          <button className="poison" onClick={ this.setPokemonByType }>Poison</button>
-          <button className="psychic" onClick={ this.setPokemonByType }>Psychic</button>
-          <button className="normal" onClick={ this.setPokemonByType }>Normal</button>
-          <button className="dragon" onClick={ this.setPokemonByType }>Dragon</button>
+          <button className="all" onClick={ setPokemonByType }>All</button>
+          <button className="electric" onClick={ setPokemonByType }>Electric</button>
+          <button className="fire" onClick={ setPokemonByType }>Fire</button>
+          <button className="bug" onClick={ setPokemonByType }>Bug</button>
+          <button className="poison" onClick={ setPokemonByType }>Poison</button>
+          <button className="psychic" onClick={ setPokemonByType }>Psychic</button>
+          <button className="normal" onClick={ setPokemonByType }>Normal</button>
+          <button className="dragon" onClick={ setPokemonByType }>Dragon</button>
         </div>
-        <button className="next-btn" onClick={ this.nextPokemon } >Próximo Pokemon</button>
+        <button className="next-btn" onClick={ nextPokemon } >Próximo Pokemon</button>
       </div>
     </div>
     );
